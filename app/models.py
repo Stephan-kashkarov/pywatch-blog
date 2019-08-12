2065668100c5a838639601c743887864970329ad
stephan-kashkarov <kashkarovwork@gmail.com>
2019-08-12 22:19:05 +1000

    added space to db

--- app/models.py
+++ app/models.py
@@ -5,6 +5,5 @@ from sqlalchemy.sql import func
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50))
-    text = db.Column(db.String(1000))
-    media = db.Column(db.String(50))
+    text = db.Column(db.String(10000))
     timestamp = db.Column(db.DateTime(timezone=True), server_default=func.now())
